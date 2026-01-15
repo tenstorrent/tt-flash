b4f27ef11f8abf263e2c083a2a58685c81084573
Petra Alexson <palexson@tenstorrent.com>
2026-01-15 11:35:50 -0500

    Add writeback_boardcfg test case for adding boardcfg data to writes
    Create a test case, test_writeback_boardcfg_adds_boardcfg_to_writes, which
    removes boardcfg data from writes if it exists, calls writeback_boardcfg, then
    checks if the current boardcfg data from the chip gets added to writes
    successfully.
    
    Signed-off-by: Petra Alexson <palexson@tenstorrent.com>

--- tests/test_tag_handlers.py
+++ tests/test_tag_handlers.py
@@ -106,3 +106,63 @@ class TestTagHandlers:
             ), "boardcfg data not preserved"
 
     # TODO: test if boardcfg is a decodable read-only protobuf?
+
+    def test_writeback_boardcfg_adds_boardcfg_to_writes(
+        self, bh_chips: list[BhChip], fwbundle_path: str
+    ):
+        """
+        BH only.
+        When using the BH tag handler writeback_boardcfg, tests that the boardcfg data
+        is added to the writes if it's not present.
+        """
+        for bh_chip in bh_chips:
+            # Read current boardcfg from chip
+            current_boardcfg_fd = boot_fs.read_tag(
+                lambda addr, size: bh_chip.spi_read(addr, size), "boardcfg"
+            )
+            assert current_boardcfg_fd is not None, "Could not find boardcfg fd on chip"
+            current_boardcfg_data = bh_chip.spi_read(
+                current_boardcfg_fd[1].spi_addr,
+                current_boardcfg_fd[1].flags.f.image_size,
+            )
+
+            # Load flash writes
+            writes = bh_load_flash_writes_from_fwbundle(bh_chip, fwbundle_path)
+
+            # Find boardcfg fd
+            writes_boardcfg_fd = None
+            for write in writes:
+                writes_boardcfg_fd = boot_fs.read_tag(
+                    lambda addr, size: write.write[addr : addr + size], "boardcfg"
+                )
+                if writes_boardcfg_fd is not None:
+                    break
+            assert (
+                writes_boardcfg_fd is not None
+            ), "Could not find boardcfg fd in flash writes"
+
+            # Remove boardcfg data write if it's there
+            for write in writes:
+                if write.offset == writes_boardcfg_fd[1].spi_addr:
+                    writes.remove(write)
+                    break
+
+            # Modify flash writes based on writeback_boardcfg
+            writes = writeback_boardcfg(bh_chip, writes)
+
+            # Find the new boardcfg data in the writes
+            new_boardcfg_data = None
+            for write in writes:
+                if write.offset == writes_boardcfg_fd[1].spi_addr:
+                    new_boardcfg_data = write.write[
+                        0 : writes_boardcfg_fd[1].flags.f.image_size
+                    ]
+            assert (
+                new_boardcfg_data is not None
+            ), "Could not find boardcfg data in modified writes"
+
+            assert (
+                new_boardcfg_data == current_boardcfg_data
+            ), "boardcfg data not preserved"
+                
+
